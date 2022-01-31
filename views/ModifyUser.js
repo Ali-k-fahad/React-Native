0698e80616ff9fa9806e5d6bdd2c102acb76e68e
Ali-k-fahad <alikareem.fahad@gmail.com>
2022-01-31 11:27:08 +0200

    extras

--- views/ModifyUser.js
+++ views/ModifyUser.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Alert, View} from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
 import {useUser} from '../hooks/ApiHooks';
 import {Input, Button} from 'react-native-elements';
+import {MainContext} from '../contexts/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {PropTypes} from 'prop-types';
 
-const RegisterForm = ({setFormToggle}) => {
-  const {postUser, checkUsername} = useUser();
-
+const ModifyUser = ({navigation}) => {
+  const {checkUsername, putUser} = useUser();
+  const {user, setUser} = useContext(MainContext);
   const {
     control,
     handleSubmit,
@@ -15,11 +17,11 @@ const RegisterForm = ({setFormToggle}) => {
     getValues,
   } = useForm({
     defaultValues: {
-      username: '',
+      username: user.username,
       password: '',
       confirmPassword: '',
-      email: '',
-      full_name: '',
+      email: user.email,
+      full_name: user.full_name,
     },
     mode: 'onBlur',
   });
@@ -28,11 +30,16 @@ const RegisterForm = ({setFormToggle}) => {
     console.log(data);
     try {
       delete data.confirmPassword;
-      const userData = await postUser(data);
-      console.log('register onSubmit', userData);
+      if (data.password === '') {
+        delete data.password;
+      }
+      const userToken = await AsyncStorage.getItem('userToken');
+      const userData = await putUser(data, userToken);
       if (userData) {
-        Alert.alert('Success', 'User created successfully.');
-        setFormToggle(true);
+        Alert.alert('Success', userData.message);
+        delete data.password;
+        setUser(data);
+        navigation.navigate('Profile');
       }
     } catch (error) {
       console.error(error);
@@ -52,7 +59,7 @@ const RegisterForm = ({setFormToggle}) => {
           validate: async (value) => {
             try {
               const available = await checkUsername(value);
-              if (available) {
+              if (available || user.username === value) {
                 return true;
               } else {
                 return 'Username is already taken.';
@@ -78,7 +85,6 @@ const RegisterForm = ({setFormToggle}) => {
       <Controller
         control={control}
         rules={{
-          required: {value: true, message: 'This is required.'},
           minLength: {
             value: 5,
             message: 'Password has to be at least 5 characters.',
@@ -107,7 +113,6 @@ const RegisterForm = ({setFormToggle}) => {
       <Controller
         control={control}
         rules={{
-          required: {value: true, message: 'This is required.'},
           validate: (value) => {
             const {password} = getValues();
             if (value === password) {
@@ -181,8 +186,8 @@ const RegisterForm = ({setFormToggle}) => {
   );
 };
 
-RegisterForm.propTypes = {
-  setFormToggle: PropTypes.func,
+ModifyUser.propTypes = {
+  navigation: PropTypes.object,
 };
 
-export default RegisterForm;
+export default ModifyUser;
